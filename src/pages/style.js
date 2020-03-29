cba52e3a8097a9d590571f7882d5c5f70c457e44
Timothee Givois <tim.givois.mendez@gmail.com>
2020-03-29 16:02:17 -0700

    Adding responsiveness

--- src/pages/style.js
+++ src/pages/style.js
@@ -4,8 +4,15 @@ import { Row } from 'react-flexbox-grid'
 import { SocialMediaIconsReact } from 'social-media-icons-react'
 
 export const Wrapper = styled(Row)`
-  padding-top: 20px;
   min-height: 65%;
+
+  @media (min-width:991px) {
+    padding: 20px 35px 0 35px;
+  }
+
+  @media (max-width:991px) {
+    padding: 20px 10px 0 10px;
+  }
 `
 
 export const StyledSocialMediaIconsReact = (props) => {
