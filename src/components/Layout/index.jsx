41787fda3d4ff55fe81cd0f924fb5d6580141d22
Tim Givois <tim.givois.mendez@gmail.com>
2021-08-13 19:09:07 -0700

    fix this

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,19 +1,22 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 import { CSSBaseline, ZEITUIProvider, useTheme } from '@zeit-ui/react'
-import { Grid, Col, Row } from 'react-flexbox-grid';
+import { Grid, Col, Row } from 'react-flexbox-grid'
 
 import './layout.css'
 import 'react-toggle/style.css'
 
 export const BarWrapper = styled(Row)`
-  box-shadow: ${({ type }) => type === 'light' ? '0 -2px rgba(0,0,0,.25)' : '0 -2px rgba(255,255,255,.25)'};
+  box-shadow: ${({ type }) =>
+    type === 'light'
+      ? '0 -2px rgba(0,0,0,.25)'
+      : '0 -2px rgba(255,255,255,.25)'};
   min-height: 70px;
   padding-top: 15px;
   margin-top: 15px;
 `
 
-const withStyle = (Component) => (props) => {
+const withStyle = Component => props => {
   const myTheme = {
     // palette: {
     //   background: '#FAFAFA',
@@ -22,36 +25,36 @@ const withStyle = (Component) => (props) => {
   }
   const [type, setType] = useState('light')
 
-  const switchTheme = (theme) => {
+  const switchTheme = theme => {
     setType(theme)
   }
 
   const theme = {}
 
-  var d = new Date();
+  var d = new Date()
 
   return (
     <ZEITUIProvider theme={Object.assign({ type })}>
-        <CSSBaseline />
-        <style>
-            @import url('https://fonts.googleapis.com/css?family=Maven+Pro&display=swap');
-        </style>
-        <Component {...props} switchTheme={switchTheme} />
-        <footer>
-            <BarWrapper type={type} center='xs'>
-              <Col xs={2}>
-                <Row>
-                  <small>&copy; {d.getFullYear()} timgivois</small>
-                </Row>
-              </Col>
-              <Col xs={4}>
-                <Row end='xs'>
-                </Row>
-              </Col>
-            </BarWrapper>
-        </footer>
+      <CSSBaseline />
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css?family=Maven+Pro&display=swap');
+      </style>
+      <Component {...props} switchTheme={switchTheme} />
+      <footer>
+        <BarWrapper type={type} center="xs">
+          <Col xs={2}>
+            <Row>
+              <small>&copy; {d.getFullYear()} timgivois</small>
+            </Row>
+          </Col>
+          <Col xs={4}>
+            <Row end="xs"></Row>
+          </Col>
+        </BarWrapper>
+      </footer>
     </ZEITUIProvider>
-    )
+  )
 }
 
 export default withStyle
